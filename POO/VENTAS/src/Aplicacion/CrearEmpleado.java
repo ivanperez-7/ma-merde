cb3c273855ce67faeccc33fdaa6d843fca23b4be
Ivan <ivan.rubik@hotmail.com>
2020-02-10 11:51:07 -0600

    hm

---
+++ POO/VENTAS/src/Aplicacion/CrearEmpleado.java
@@ -0,0 +1,9 @@
+package Aplicacion;
+
+import Modelo.EmpleadoPorComision;
+
+public class CrearEmpleado{
+    public static void main(String[] args){
+        EmpleadoPorComision ejemplo = new EmpleadoPorComision("15004196","ivan","perez","maldonado",0.10,2010.0);
+    }
+}
---
+++ POO/VENTAS/src/Modelo/EmpleadoPorComision.java
@@ -0,0 +1,45 @@
+public class EmpleadoPorComision{
+    private String numSeguroSocial;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private double tarifaComision;
+    private double ventasBrutas;
+
+    public EmpleadoPorComision(String numSeguroSocial, String nombre, String apellidoPaterno, String apellidoMaterno, double tarifaComision, double ventasBrutas){
+        this.numSeguroSocial = numSeguroSocial;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.tarifaComision = tarifaComision;
+        this.ventasBrutas = ventasBrutas;
+    }
+
+    public String getNumSeguroSocial(){
+        return numSeguroSocial;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellidoMaterno(){
+        return apellidoPaterno;
+    }
+
+    public String getApellidoMaterno(){
+        return apellidoMaterno;
+    }
+
+    public double getTarifaComision(){
+        return tarifaComision;
+    }
+
+    public double getVentasBrutas(){
+        return ventasBrutas;
+    }
+
+    public double ingreso(){
+        return getTarifaComision * getVentasBrutas();
+    }
+}
