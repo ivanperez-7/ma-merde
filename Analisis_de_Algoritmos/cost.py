ffd2e7abd01683b441aba7ef6198ff3933fd2cc3
Iván Pérez <ivan.rubik@hotmail.com>
2020-03-22 02:44:20 -0600

    hm

--- Analisis_de_Algoritmos/cost.py
+++ Analisis_de_Algoritmos/cost.py
@@ -1,29 +1,16 @@
 def costR(r,i,j):
     if i == j:
-        return 0
+        return 0 
     
-    min = 10**20
-    for k in range(i,j):
-        count = costR(r,i,k) + costR(r,k+1,j) + r[i-1]*r[k]*r[j]
-        if count < min:
-            min = count
-    return min
+    return min([costR(r,i,k) + costR(r,k+1,j) + r[i-1]*r[k]*r[j] for k in range(i,j)])
 
 def cost(r,n):
     m = [[0 for i in range(n+1)] for i in range(n+1)]
 
-    for i in range(1,n+1):
-        m[i][i] = 0
     for d in range(1,n):
         for i in range(1,n-d+1):
             j = i+d
-            min = 10**20
-
-            for k in range(i,j):
-                count = m[i][k] + m[k+1][j] + r[i-1]*r[k]*r[j]
-                if count < min:
-                    min = count
-            m[i][j] = min
+            m[i][j] = min([m[i][k] + m[k+1][j] + r[i-1]*r[k]*r[j] for k in range(i,j)])
     return m[1][n]
 
 r = [1,2,4,4,3,2]
